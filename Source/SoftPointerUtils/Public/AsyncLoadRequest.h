de5730ed674d4d33dcaf55e511f75ae40197dbc4
FrantisekSumsala <43442115+FrantisekSumsala@users.noreply.github.com>
2023-09-24 23:10:02 +0200

    Created a custom async load request object as a wrapper around FStreamableHandle for simpler C++ API and blueprint usability (pending testing)

---
+++ Source/SoftPointerUtils/Public/AsyncLoadRequest.h
@@ -0,0 +1,85 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "AsyncLoadRequest.generated.h"
+
+struct FStreamableHandle;
+
+UENUM(BlueprintType)
+enum class EAsyncLoadRequestStatus : uint8
+{
+	InProgress,
+	Stalled,
+	Completed,
+	Canceled,
+	Active
+};
+
+DECLARE_DYNAMIC_DELEGATE(FAsyncLoadDelegate);
+
+UCLASS(BlueprintType)
+class SOFTPOINTERUTILS_API UAsyncLoadRequest : public UObject
+{
+	GENERATED_BODY()
+
+	friend class USoftPointerUtilsLibrary;
+	
+private:
+	TSharedPtr<FStreamableHandle> LoadHandle;
+
+	UPROPERTY()
+	FAsyncLoadDelegate OnCompleteDelegate;
+
+	UPROPERTY()
+	FAsyncLoadDelegate OnCancelDelegate;
+
+public:
+	UFUNCTION(BlueprintPure)
+	EAsyncLoadRequestStatus GetStatus() const;
+
+	UFUNCTION(BlueprintPure)
+	float GetProgress() const;
+	// GetProgress
+	
+	UFUNCTION(BlueprintCallable)
+	bool BindOnCompleteDelegate(const FAsyncLoadDelegate& Delegate);
+	// BindCompleteDelegate
+
+	UFUNCTION(BlueprintCallable)
+	bool BindOnCancelDelegate(const FAsyncLoadDelegate& Delegate);
+	// BindCancelDelegate
+	
+	UFUNCTION(BlueprintCallable)
+	void ForceLoad(const float Timeout = 0.0f, const bool bStartStalledHandles = true) const;
+	// WaitUntilComplete
+	
+	UFUNCTION(BlueprintCallable)
+	void CancelLoadRequest() const;
+	// CancelHandle
+	
+	//TODO: non-wrapped functions - verify that we have wrapped all relevant functions
+	// - BindUpdateDelegate - could not be bound for some reason and seemed like would not be invoked anyway
+	// - HasUpdateDelegate - pointless without BindUpdateDelegate
+	// - ReleaseHandle - weirder version of CancelHandle, probably more suited for internal purposes and thus redundant
+	// - IsActive - replaced by unifying GetStatus()
+	// - IsLoadingInProgress - replaced by unifying GetStatus()
+	// - IsStalled - replaced by unifying GetStatus()
+	// - HasLoadCompleted - replaced by unifying GetStatus()
+	// - WasCanceled - replaced by unifying GetStatus()
+	// - GetRequestedAssets - seems like intended for internal purposes
+	// - GetLoadedAssets - seems like intended for internal purposes
+	// - HasCompleteDelegate - seems kind of pointless
+	// - HasCancelDelegate - seems kind of pointless
+
+	TSharedPtr<FStreamableHandle> GetLoadHandle() const;
+	
+private:
+	void Initialize(TSharedPtr<FStreamableHandle> InWrappedHandle);
+	
+	void OnComplete();
+	void OnCancel();
+
+};
